cd6245e998e0d47c520015839c8a141b3af388e9
Mohammed Furkan <furkanrangrej200@gmail.com>
2023-10-17 17:06:53 +0530

    host web

--- client/src/components/Backend_ref.jsx
+++ client/src/components/Backend_ref.jsx
@@ -5,6 +5,6 @@ if (process.env.NODE_ENV  === 'production') {
 else if (process.env.NODE_ENV  === 'development') {
     backend_ref = "http://localhost:5000"
 }
-// backend_ref = "https://bandhejhub.onrender.com"
+backend_ref = "https://bandhejhub.onrender.com"
 
 export default backend_ref
