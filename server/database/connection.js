d48d402d7c69b947e93847ef6cf0aec3c4348318
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-01-23 10:38:39 +0530

    host 4

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,14 +1,14 @@
-// const mongoose = require('mongoose');
-// const DB = process.env.DB_NAME;
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("connection successful");
-//   })
-//   .catch((err) => console.log("database connection", err));
+const mongoose = require('mongoose');
+const DB = process.env.DB_NAME;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log("database connection", err));
 
 
 //   const mongoose = require('mongoose');
@@ -21,12 +21,12 @@
 //   console.log('MongoDB database connection established successfully');
 // });
  
-const mongoose = require('mongoose');
-const mongoURI = process.env.DB_NAME;
+// const mongoose = require('mongoose');
+// const mongoURI = process.env.DB_NAME;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully');
+// });
