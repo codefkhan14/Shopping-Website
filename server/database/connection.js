c7feb19358766cfb39cc7cb8a89055ffb4047f3c
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-01-23 18:48:00 +0530

    host12

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
-const DB = process.env.DB_NAME;
+// const DB = process.env.DB_NAME;
+const DB =
+  "mongodb+srv://furkanrangrej200:furkan123@cluster0.yoqmgpi.mongodb.net/BandhejHub?retryWrites=true&w=majority";
+
 mongoose
   .connect(DB, {})
   .then(() => {
