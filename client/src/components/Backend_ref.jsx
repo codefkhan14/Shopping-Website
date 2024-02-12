10922ddd4c61cca14d18a00c6575087309a498eb
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-02-13 00:00:43 +0530

    add more colors

--- client/src/components/Backend_ref.jsx
+++ client/src/components/Backend_ref.jsx
@@ -1,9 +1,9 @@
 let backend_ref;
-// backend_ref = process.env.REACT_APP_HOST_SERVER_KEY;
-// if (process.env.NODE_ENV === "production") {
-//   backend_ref = process.env.REACT_APP_HOST_SERVER_KEY;
-// } else if (process.env.NODE_ENV === "development") {
+backend_ref = process.env.REACT_APP_HOST_SERVER_KEY;
+if (process.env.NODE_ENV === "production") {
+  backend_ref = process.env.REACT_APP_HOST_SERVER_KEY;
+} else if (process.env.NODE_ENV === "development") {
   backend_ref = process.env.REACT_APP_LOCAL_SERVER_KEY;
-// }
+}
 
 export default backend_ref;
