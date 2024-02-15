13e4b66609cdd5647a3e2e34e6f83d298709288b
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-02-15 16:22:38 +0530

    add carousel

---
+++ client/src/components/CarouselCmp.jsx
@@ -0,0 +1,44 @@
+import React, {  useState } from "react";
+import "../style/CarouselCmp.css";
+
+const CarouselCmp = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     const newIndex = (currentIndex - 1 + images.length) % images.length;
+  //     setCurrentIndex(newIndex);
+  //   }, 5000);
+  // });
+
+  // dssdv
+  const goToPrevSlide = () => {
+    const newIndex = (currentIndex - 1 + images.length) % images.length;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNextSlide = () => {
+    const newIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(newIndex);
+  };
+
+  return (
+    <div className="carousel-container">
+      <button className="prev-button" onClick={goToPrevSlide}>
+        Prev
+      </button>
+
+      <div className="carousel">
+        <div className="slide active">
+          <img src={images[currentIndex]} alt="loading..." />
+        </div>
+      </div>
+
+      <button className="next-button" onClick={goToNextSlide}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default CarouselCmp;
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -8,6 +8,7 @@ import {
   GET_PRODUCT_BY_TAG,
   GET_PRODUCT_BY_CATEGORY,
 } from "../components/Apis";
+import CarouselCmp from "../components/CarouselCmp";
 
 function Home() {
   const [topTrending, setTopTrending] = useState(null);
@@ -87,10 +88,19 @@ function Home() {
     };
     getProductByLehanga();
   }, []);
+
+  const images = [
+    "https://amounee.com/cdn/shop/files/product-banner_1800x.jpg?v=1652165756",
+    "https://www.onlinebandhej.com/cdn/shop/files/Bandhani_Banner_New_copy_c4f909c7-047f-4f5b-bd1b-874c17059812_2048x.jpg?v=1704643022",
+    "https://www.onlinebandhej.com/cdn/shop/files/Best_Seller_Bannenr_2048x.jpg?v=1704626298",
+    // Add more image URLs as needed
+  ];
+
   return (
     <>
       <TopHeader />
       <Navbar />
+      <CarouselCmp images={images} />
       <Product
         heading="Top Trending"
         summary="Collections Of Top Trending Items "
