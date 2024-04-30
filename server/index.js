9c73d90c36f2526288a220bf542f12e4a82970b3
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-04-30 18:02:50 +0530

    test2

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const axios = require("axios");
 const cors = require("cors");
 const Razorpay = require("razorpay");
 dotenv.config();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://bandhejhub.netlify.app"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
