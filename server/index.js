e5b23e7991b5af687825a89344bd318fa8fb544a
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-04-30 17:08:48 +0530

    remove bugs

--- server/index.js
+++ server/index.js
@@ -17,34 +17,4 @@ app.use(require("./router/payment"));
 app.use(require("./router/admin"));
 app.use(require("./router/product"));
 
-// const instance = new Razorpay({
-//   key_id: "rzp_test_VIMg5R33m4Tjpx",
-//   key_secret: "FEKNXvr9Kr8xEfKckNIKh7jr",
-// });
-
-// var instance = new Razorpay({
-//   key_id: "rzp_test_VIMg5R33m4Tjpx",
-//   key_secret: "FEKNXvr9Kr8xEfKckNIKh7jr",
-// });
-
-// instance.orders.create({
-//   amount: 50000,
-//   currency: "INR",
-//   // receipt: "receipt#1",
-//   // notes: {
-//   //   key1: "value3",
-//   //   key2: "value2",
-//   // },
-// });
-
-// module.exports = instance;
 app.listen(process.env.PORT);
-
-// function pingLink() {
-//   const linkToPing = "https://bandhejhub.onrender.com/furkan";
-//   let data = axios.get(linkToPing);
-//   data.then((res) => {});
-// }
-
-// const pingInterval = 11 * 60 * 1000;
-// setInterval(pingLink, pingInterval);
