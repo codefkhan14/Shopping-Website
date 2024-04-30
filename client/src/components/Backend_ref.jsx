217c45e9653a69adb59ec309b5b80a1b7f075fec
Mohammed Furkan <furkanrangrej200@gmail.com>
2024-04-30 18:10:18 +0530

    cors error resolve

--- client/src/components/Backend_ref.jsx
+++ client/src/components/Backend_ref.jsx
@@ -1,5 +1,5 @@
 let backend_ref;
-// backend_ref = process.env.REACT_APP_HOST_SERVER_KEY;
+
 if (process.env.NODE_ENV === "production") {
   backend_ref = process.env.REACT_APP_HOST_SERVER_KEY;
 } else if (process.env.NODE_ENV === "development") {
